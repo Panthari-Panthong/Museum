84b6ea7dd82ae1059cbb78e14f94e89322e17047
Panthari <YOUR@EMAIL.com>
2019-09-19 13:45:40 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,8 +1,8 @@
 function submitComment() {
   let inputField = document.querySelector('#name');
   let name = inputField.value;
-  let inputComment = document.querySelector('#msg');
-  let msg = inputComment.value;
+  let textArea = document.querySelector('#msg');
+  let msg = textArea.value;
 
   let comment = document.createElement('section');
   let h3 = document.createElement('h3');
@@ -16,6 +16,12 @@ function submitComment() {
   comment.appendChild(h3);
   comment.appendChild(p);
 
-  console.log(comment);
 
+  let commentSection = document.querySelector('#comments')
+
+
+  commentSection.appendChild(comment);
+
+  inputField.value = null;
+  textArea.value = null;
 }
