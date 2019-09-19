47f86d020839397772c277bcd0fe495bfe4291b1
Panthari <YOUR@EMAIL.com>
2019-09-19 12:31:17 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,21 @@
 function submitComment() {
-  console.log("Hello!");
+  let inputField = document.querySelector('#name');
+  let name = inputField.value;
+  let inputComment = document.querySelector('#msg');
+  let msg = inputComment.value;
+
+  let comment = document.createElement('section');
+  let h3 = document.createElement('h3');
+  let p = document.createElement('p');
+
+  h3.textContent = `${name} said:`
+  p.textContent = msg;
+
+
+  comment.classList.add('comment');
+  comment.appendChild(h3);
+  comment.appendChild(p);
+
+  console.log(comment);
+
 }
